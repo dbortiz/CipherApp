700dab98b9339a180cbeaa8cb776e9e64eb75e47
dbortiz <dbortiz@csu.fullerton.edu>
2021-02-16 17:55:48 -0800

    Finished key selection panel.

---
+++ SelectKeyPanel.java
@@ -0,0 +1,43 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class SelectKeyPanel extends JPanel{
+    SelectKeyPanel() {
+        // Creates layout for panel
+        this.setLayout(new FlowLayout());
+
+        // Components for key selection
+        final JLabel label = new JLabel("Enter key: ");
+        final JPasswordField key = new JPasswordField(10);
+        key.setEchoChar('*');
+
+        // Resize password viewer
+        ImageIcon icon = new ImageIcon("password_icon.png", "Toggable Password Viewer");
+        Image image = icon.getImage();
+        Image newimg = image.getScaledInstance(20,20, java.awt.Image.SCALE_SMOOTH);
+        icon = new ImageIcon(newimg);
+        final JLabel view = new JLabel(icon);
+        
+        // Hovering over icon will allow for user to view currently typed key
+        view.addMouseListener(new MouseAdapter () {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                key.setEchoChar((char)0);
+            }
+        });
+
+        // While not hovering over icon, key will be defaulted to '*'
+        view.addMouseListener(new MouseAdapter () {
+            @Override
+            public void mouseExited(MouseEvent e) {
+                key.setEchoChar('*');
+            }
+        });
+
+        // Add components to panel
+        this.add(label);
+        this.add(key);
+        this.add(view);
+    }
+}
